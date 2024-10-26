0f7e76f7e644ea0b06c0ab2e55b8a7bf625d3983
kkolcz <k.kolcz@outlook.com>
2024-10-26 15:53:16 +0200

    changes in styles

--- Frontend/src/app/pages/user-profile/user-profile.component.ts
+++ Frontend/src/app/pages/user-profile/user-profile.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+interface Order {
+  id: number;
+  date: string;
+  status: string;
+}
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.scss']
+  styleUrls: ['./user-profile.component.scss'],
 })
 export class UserProfileComponent {
+  userForm: FormGroup;
+  editMode = false;
 
+  constructor(private fb: FormBuilder) {
+    this.userForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.minLength(2)]],
+      lastName: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required, Validators.pattern(/^\d{9}$/)]],
+      address: ['', Validators.required],
+    });
+  }
+
+  toggleEdit() {
+    this.editMode = !this.editMode;
+  }
+
+  saveChanges() {
+    if (this.userForm.valid) {
+      console.log('User data saved:', this.userForm.value);
+      this.editMode = false;
+    } else {
+      console.error('Invalid form data');
+    }
+  }
 }
