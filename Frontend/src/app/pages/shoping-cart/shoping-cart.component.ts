e99c7e9665c5e273af95139a646301b2b621291e
kkolcz <k.kolcz@outlook.com>
2024-10-26 15:19:48 +0200

    added shoping card

--- Frontend/src/app/pages/shoping-cart/shoping-cart.component.ts
+++ Frontend/src/app/pages/shoping-cart/shoping-cart.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { IProduct } from 'src/app/shared/models/product.model';
 
 @Component({
   selector: 'app-shoping-cart',
   templateUrl: './shoping-cart.component.html',
-  styleUrls: ['./shoping-cart.component.scss']
+  styleUrls: ['./shoping-cart.component.scss'],
 })
 export class ShopingCartComponent {
-
+  productsInCart: IProduct[] = [
+    {
+      id: 1,
+      name: 'Nothing Phone',
+      price: '2499.00',
+      photos: ['http://localhost:8080/api/v1/product/images/nothing1.jpg'],
+      mainPhotoId: 1,
+      productCategory: { id: 1, name: 'Telefony' },
+      description: 'Telefon Nothing Phone',
+    },
+  ];
 }
