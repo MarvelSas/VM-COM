0f3259fa8ef5a3fe0dc6c037da1afa98a837bf8a
kkolcz <k.kolcz@outlook.com>
2024-12-09 03:08:36 +0100

    added user profile with dummy data

--- Frontend/src/app/pages/user-profile/user-profile.component.ts
+++ Frontend/src/app/pages/user-profile/user-profile.component.ts
@@ -1,28 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
-interface Order {
-  id: number;
-  date: string;
-  status: string;
-}
+import { UserService } from 'src/app/shared/services/user.service';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.scss'],
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements OnInit {
   userForm: FormGroup;
   editMode = false;
+  userData: any = {};
+  userAddress: any = {};
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private userService: UserService) {
     this.userForm = this.fb.group({
       firstName: ['', [Validators.required, Validators.minLength(2)]],
       lastName: ['', [Validators.required, Validators.minLength(2)]],
       email: ['', [Validators.required, Validators.email]],
       phone: ['', [Validators.required, Validators.pattern(/^\d{9}$/)]],
       address: ['', Validators.required],
+    });
+  }
+
+  ngOnInit(): void {
+    this.getUserData();
+  }
+
+  getUserData() {
+    this.userService.getUserData().subscribe({
+      next: (data) => {
+        this.userData = data;
+        this.userForm.patchValue({
+          firstName: data.name,
+          lastName: data.surname,
+          email: data.email,
+          phone: data.phone,
+        });
+      },
+      error: (error) => {
+        console.error('Error fetching user data:', error);
+      },
+      complete: () => {},
+    });
+
+    this.userService.getUserAddress().subscribe({
+      next: (data) => {
+        this.userAddress = data;
+        this.userForm.patchValue({
+          address: `${data.street} ${data.number}, ${data.city}, ${data.zip}`,
+        });
+      },
+      error: (error) => {
+        console.error('Error fetching user address:', error);
+      },
     });
   }
 
--- Frontend/src/app/shared/services/user.service.ts
+++ Frontend/src/app/shared/services/user.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  dummyUserData = {
+    name: 'John',
+    surname: 'Doe',
+    email: 'test@mail.com',
+    phone: '123456789',
+  };
+  dummyAddressData = {
+    city: 'Warsaw',
+    street: 'Marszałkowska',
+    number: '1',
+    zip: '00-000',
+  };
 
-  constructor() { }
+  constructor() {}
+
+  getUserData(): Observable<any> {
+    return of(this.dummyUserData);
+  }
+
+  updateUserData() {}
+
+  getUserAddress(): Observable<any> {
+    return of(this.dummyAddressData);
+  }
 }
