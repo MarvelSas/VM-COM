70cda21ce95c0c5c6b3986cc21d912e98f10c32a
kkolcz <k.kolcz@outlook.com>
2025-01-11 23:02:10 +0100

    added errors for email and password

--- Frontend/src/app/pages/login/login.component.ts
+++ Frontend/src/app/pages/login/login.component.ts
@@ -22,7 +22,7 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
     this.signInForm = new FormGroup({
-      email: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
       password: new FormControl(null, Validators.required),
     });
   }
